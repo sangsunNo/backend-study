0529554949e3bcfd9083638520922bc8fd830781
norii97 <mbhb8817@gmail.com>
2022-08-11 17:39:17 +0900

    update-pages

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
+// // 파일을 찾을 때에 최상단에 public/을 추가해준다
+// app.use(express.static('public'));
+// // 파일을 찾을 때에 /static => public 으로 치환해서 준다
+// app.use('/static', express.static('public'))
+
+
 const express = require("express") // express 불러온다
 const app = express(); // express 모듈을 app이라는 변수에 담는다
+app.set("view engine", "ejs")
+app.use("/static", express.static("static"))
 
-app.set("view engine", "ejs");
-
-app.use("/static", express.static("static"));
-
+// localhost:8000/
 const port = 8000;
-// ip:8000/
 
 // app.get("/", (req, res) => {
 //     // res.send("heooool");
@@ -14,14 +18,24 @@ const port = 8000;
 // });
 
 app.get("/", (req, res) => {
-    res.render("test");
+    var person = [
+        { name: "댜재라", gender: "재룯" },
+        { name: "미재답", gender: "히티" }
+    ]
+    res.render("test", { per: person });
 })
+
+// localhost:8000/test
+app.get("/test", (req, res) => {
+    res.render("test1");
+})
+
+app.get("/test1", (req, res) =>{
+    res.render("test2");
+})
+
+
 
 app.listen(port, () => {
     console.log("server open: ", port);
-});
-
-// // 파일을 찾을 때에 최상단에 public/을 추가해준다
-// app.use(express.static('public'));
-// // 파일을 찾을 때에 /static => public 으로 치환해서 준다
-// app.use('/static', express.static('public'))
+})
